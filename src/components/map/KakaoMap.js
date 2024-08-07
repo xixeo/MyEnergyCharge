b8281e3e14c955f91fdd2878353184e1f03be67a
xixeo <xixeo0121@gmail.com>
2024-08-07 11:38:37 +0900

    지도 완성

--- src/components/map/KakaoMap.js
+++ src/components/map/KakaoMap.js
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from "react";
 import siguData from "../data/sig.json"; // SIG 데이터 JSON 파일 경로
 import sidoData from "../data/sido.json"; // 시도 데이터 JSON 파일 경로
 
-// 지도 레벨에 따라 행정구역 다르게 표현
 // 아이콘 파일을 모듈처럼 가져오기
 import w01 from "../../assets/icons/w01.png";
 import w02 from "../../assets/icons/w02.png";
@@ -18,7 +17,13 @@ const KakaoMap = ({ onMapReady, area, subArea }) => {
     const [subAreaName, setSubAreaName] = useState(""); // 하위 지역 이름 상태
     const [weather] = useState("맑음"); // 날씨 상태 기본값
     const polygons = useRef([]); // 폴리곤을 저장할 useRef 변수
-    const [currentZoomLevel, setCurrentZoomLevel] = useState(); // 초기 줌 레벨 상태 설정
+    const [currentZoomLevel, setCurrentZoomLevel] = useState(9); // 초기 줌 레벨 상태 설정
+    // 색상 배열 정의
+    const colorPalette = [
+        "#FFDDC1", "#C1E1C1", "#C1D3E1", "#FAD02E", "#F28D35", "#E56B6F",
+        '#D30C02', '#F6AA55', '#F7F7B6', '#A7D3E4', '#2275B2', '#F769AA',
+        '#F7D592', '#CFF77F', '#6CF2F7', '#95B4F7',
+    ];
 
     // 지도 초기화 및 폴리곤 추가
     useEffect(() => {
@@ -39,34 +44,11 @@ const KakaoMap = ({ onMapReady, area, subArea }) => {
                     onMapReady(map); // 지도 준비 완료 시 콜백 호출
                 }
 
-               // 지도 줌 컨트롤을 생성
+                // 지도 줌 컨트롤을 생성
                 const zoomControl = new kakao.maps.ZoomControl();
                 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
                 // 마우스로 지도 줌 하는거 막기
                 map.setZoomable(false);
-
-                // SIGU 데이터로 폴리곤 추가
-                const addPolygons = () => {
-                    deletePolygon(); // 기존 폴리곤 삭제
-                    const dataToUse =
-                        currentZoomLevel < 11 ? siguData : sidoData;
-                    dataToUse.features.forEach((feature) => {
-                        getPolycode(feature, map);
-                    });
-                };
-
-                addPolygons(); // 초기 폴리곤 추가
-
-                // 지도 줌 레벨 변경 이벤트 리스너 추가
-                kakao.maps.event.addListener(map, "zoom_changed", () => {
-                    const level = map.getLevel();
-                    setCurrentZoomLevel(level);
-                    if (level <= 8) {
-                        deletePolygon(); // 레벨이 8 이하일 때 폴리곤 삭제
-                    } else {
-                        addPolygons(); // 레벨이 8 이상일 때 폴리곤 추가
-                    }
-                });
 
                 // 지도 클릭 이벤트 리스너 추가
                 kakao.maps.event.addListener(map, "click", (mouseEvent) => {
@@ -90,7 +72,13 @@ const KakaoMap = ({ onMapReady, area, subArea }) => {
                         } else {
                             console.error("주소 변환 실패:", status);
                         }
-                    });                    
+                    });
+                });
+
+                // 지도 줌 레벨 변경 이벤트 리스너 추가
+                kakao.maps.event.addListener(map, "zoom_changed", () => {
+                    const level = map.getLevel();
+                    setCurrentZoomLevel(level);
                 });
 
                 // 초기 줌 레벨 상태 설정
@@ -108,7 +96,7 @@ const KakaoMap = ({ onMapReady, area, subArea }) => {
     };
 
     // 폴리곤 추가 함수
-    const getPolycode = (feature, map) => {
+    const getPolycode = (feature, map, featureIndex) => {
         const { geometry, properties } = feature;
         let path = [];
         let coordinates = geometry.coordinates[0];
@@ -125,14 +113,14 @@ const KakaoMap = ({ onMapReady, area, subArea }) => {
             coordinates.forEach((coord) => {
                 path.push(new kakao.maps.LatLng(coord[1], coord[0]));
             });
-            setPolygon({ name, path }, map);
+            setPolygon({ name, path, featureIndex }, map); // featureIndex 추가
         } else if (geometry.type === "MultiPolygon") {
             coordinates.forEach((polygon) => {
                 path = [];
                 polygon[0].forEach((coord) => {
                     path.push(new kakao.maps.LatLng(coord[1], coord[0]));
                 });
-                setPolygon({ name, path }, map);
+                setPolygon({ name, path, featureIndex }, map); // featureIndex 추가
             });
         }
     };
@@ -144,16 +132,16 @@ const KakaoMap = ({ onMapReady, area, subArea }) => {
             strokeWeight: 2,
             strokeColor: "#004c80",
             strokeOpacity: 0.8,
-            fillColor: "#fff",
+            fillColor: colorPalette[area.featureIndex % colorPalette.length], // 색상 배열에서 색상 선택
             fillOpacity: 0.7,
         });
 
         kakao.maps.event.addListener(polygon, "mouseover", () => {
-            polygon.setOptions({ fillColor: "#09f" });
+            polygon.setOptions({ fillOpacity: 0 });
         });
 
         kakao.maps.event.addListener(polygon, "mouseout", () => {
-            polygon.setOptions({ fillColor: "#fff" });
+            polygon.setOptions({ fillColor: colorPalette[area.featureIndex % colorPalette.length], fillOpacity: 0.7 });
         });
 
         const centroidPosition = centroid(area.path); // 폴리곤의 중심 좌표 계산
@@ -161,16 +149,6 @@ const KakaoMap = ({ onMapReady, area, subArea }) => {
         // 폴리곤을 지도에 추가
         polygon.setMap(map);
         polygons.current.push(polygon); // 폴리곤을 현재 폴리곤 배열에 추가
-
-        // 항상 오버레이 설정
-        // const content = `<div class='overlaybox'>${area.name}</div>`;
-        // const newOverlay = new kakao.maps.CustomOverlay({
-        //   position: centroidPosition, // 폴리곤의 중심 좌표
-        //   content: content,
-        //   xAnchor: 0.5,
-        //   yAnchor: 0.5,
-        // });
-        // newOverlay.setMap(map);
     };
 
     // 폴리곤의 중심 좌표 계산 함수
@@ -184,6 +162,21 @@ const KakaoMap = ({ onMapReady, area, subArea }) => {
         });
         return new kakao.maps.LatLng(y / n, x / n);
     };
+
+    // 폴리곤 데이터 업데이트
+    useEffect(() => {
+        if (mapInstance) {
+            const addPolygons = () => {
+                deletePolygon(); // 기존 폴리곤 삭제
+                const dataToUse = currentZoomLevel < 11 ? siguData : sidoData;
+                dataToUse.features.forEach((feature, index) => {
+                    getPolycode(feature, mapInstance, index); // 인덱스 추가
+                });
+            };
+
+            addPolygons(); // 폴리곤 추가
+        }
+    }, [mapInstance, currentZoomLevel]);
 
     // 날씨 정보가 변경될 때 사용자 정의 오버레이 업데이트
     useEffect(() => {
