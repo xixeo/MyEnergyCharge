e4311bcc13449e547c3a60b614604aa18115ba1e
xixeo <xixeo0121@gmail.com>
2024-08-20 22:05:00 +0900

    회원가입 수정

--- src/views/Join.js
+++ src/views/Join.js
@@ -12,6 +12,7 @@ export default function Join() {
     });
     const [showPopup, setShowPopup] = useState(false); // 팝업 상태 추가
     const { showAlert } = useAlert();
+    const [popupMessage, setPopupMessage] = useState("");
     const navigate = useNavigate(); // 페이지 이동을 위한 훅
 
     const handleChange = (e) => {
@@ -24,33 +25,41 @@ export default function Join() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+    
         if (!formData.username || !formData.email || !formData.password) {
             showAlert("모든 필드를 입력해주세요.", "error");
             return;
         }
-
+    
         try {
-          const response = await fetch("http://192.168.0.144:8080/signup", {
-              method: "POST",
-              headers: {
-                  "Content-Type": "application/json",
-              },
-              body: JSON.stringify(formData),
-          });
-  
-          const result = await response.text();
-  
-          if (response.ok) {
-              showAlert("회원가입이 완료되었습니다", "success");
-          } else {
-              showAlert(result, "error");
-          }
-      } catch (error) {
-          console.error("회원가입 요청 중 오류가 발생했습니다.", error);
-          showAlert("회원가입 요청 중 오류가 발생했습니다.", "error");
-      }
-    };
+            const response = await fetch("http://localhost:8080/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+    
+            const result = await response.text();
+    
+            if (response.ok) {
+                if (result.includes("님, 가입을 축하합니다!")) {
+                    // 성공 메시지 처리
+                    setPopupMessage(result); // 팝업 메시지 설정
+                    setShowPopup(true); // 성공 메시지 팝업 표시
+                } else {
+                    // 실패 메시지 처리
+                    showAlert(result, "error");
+                }
+            } else {
+                showAlert(result, "error");
+            }
+        } catch (error) {
+            console.error("회원가입 요청 중 오류가 발생했습니다.", error);
+            showAlert("회원가입 요청 중 오류가 발생했습니다.", "error");
+        }
+    };   
+    
 
     const handleNavigateToLogin = () => {
         setShowPopup(false); // 팝업 닫기
@@ -58,88 +67,85 @@ export default function Join() {
     };
 
     return (
-      <div
-      className={`w-full h-screen ${styles.join_wrap} flex justify-center items-center`}
-  >
-      <div className="w-1/4 min-w-96 py-6 min-h-96 rounded-lg bg-white/[.55] shadow-lg flex flex-col justify-center items-center">
-          <p className="text-[#364f9b] text-3xl font-bold w-full px-8">
-              Sign up
-          </p>
-          <form
-              className="w-full px-8 pt-12 space-y-4 md:space-y-6"
-              onSubmit={handleSubmit}
-          >
-              <div className="mb-4 w-full">
-                  <label
-                      htmlFor="id"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                      ID
-                  </label>
-                  <input
-                      id="username"
-                      type="text"
-                      name="username"
-                      value={formData.username}
-                      onChange={handleChange}
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="ID"
-                  />
-              </div>
-
-              <div className="mb-4 w-full">
-                  <label
-                      htmlFor="email"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                      이메일
-                  </label>
-                  <input
-                      id="email"
-                      type="email"
-                      name="email"
-                      value={formData.email}
-                      onChange={handleChange}
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="e-mail"
-                  />
-              </div>
-
-              <div className="mb-4 w-full">
-                  <label
-                      htmlFor="password"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                      비밀번호
-                  </label>
-                  <input
-                      id="password"
-                      type="password"
-                      name="password"
-                      value={formData.password}
-                      onChange={handleChange}
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="password"
-                  />
-              </div>
-
-              <div className="py-1" />
-              <button
-                  type="submit"
-                  className="w-full text-white bg-[#343077] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                  회원가입
-              </button>
-          </form>
+        <div
+            className={`w-full h-screen ${styles.join_wrap} flex justify-center items-center`}
+        >
+            <div className="w-1/4 min-w-96 py-6 min-h-96 rounded-lg bg-white/[.55] shadow-lg flex flex-col justify-center items-center">
+                <p className="text-[#364f9b] text-3xl font-bold w-full px-8">
+                    Sign up
+                </p>
+                <form
+                    className="w-full px-8 pt-12 space-y-4 md:space-y-6"
+                    onSubmit={handleSubmit}
+                >
+                    <div className="mb-4 w-full">
+                        <label
+                            htmlFor="username"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                            ID
+                        </label>
+                        <input
+                            id="username"
+                            type="text"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleChange}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="ID"
+                        />
+                    </div>
+    
+                    <div className="mb-4 w-full">
+                        <label
+                            htmlFor="email"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                            이메일
+                        </label>
+                        <input
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="e-mail"
+                        />
+                    </div>
+    
+                    <div className="mb-4 w-full">
+                        <label
+                            htmlFor="password"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                            비밀번호
+                        </label>
+                        <input
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="password"
+                        />
+                    </div>
+    
+                    <div className="py-1" />
+                    <button
+                        type="submit"
+                        className="w-full text-white bg-[#343077] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    >
+                        회원가입
+                    </button>
+                </form>
             </div>
-
-            {/* 팝업 구현 */}
+    
             {showPopup && (
-                <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
-                    <div className="bg-white p-6 rounded-lg shadow-lg">
-                        <p className="text-xl font-bold">
-                            {formData.username}님, 가입해주셔서 감사합니다!
-                        </p>
+                <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-60">
+                    <div className="bg-white w-80 text-center px-4 py-10 rounded-lg shadow-lg">
+                        <p className="text-xl font-bold pb-10">{popupMessage}</p>
                         <button
                             onClick={handleNavigateToLogin}
                             className="mt-4 bg-[#343077] text-white px-4 py-2 rounded"
@@ -151,4 +157,5 @@ export default function Join() {
             )}
         </div>
     );
+    
 }
