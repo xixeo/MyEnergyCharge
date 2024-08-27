358ef6ab70bcf10ed43b342b42bc53da93d250f6
YISEO <75659048+xixeo@users.noreply.github.com>
2024-08-27 10:51:05 +0900

    Delete src directory

--- src/views/layouts/AtomN.js
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const AtomN = atom({
-    key: "AtomN",
-    default: ''    
-});
--- src/views/layouts/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  if (window.location.pathname === '/') return null;
-    return (
-        <footer className="bg-black text-gray-50 p-2 text-xs">FOOTER</footer>
-    );
-}
